958b690ea033926411d4e93fe9da5307a917944c
reddevilcero <reddevil_cero@hotmail.com>
2020-06-19 18:42:28 +0100

    add Dev config

---
+++ src/components/firstComponent.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class firstComponent extends Component {
+  render() {
+    return (
+      <div>
+        <img src="" alt=""/>
+      </div>
+    )
+  }
+}
