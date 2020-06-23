4d2ffb038e3e1cb5bdfd71d72bb88c0f497fbbce
reddevilcero <reddevil_cero@hotmail.com>
2020-06-23 13:27:44 +0100

    add router and links to login and signup

--- src/App.js
+++ src/App.js
@@ -3,16 +3,25 @@ import "./assets/styles/App.scss";
 // import FirstComponent from "./components/FirstComponent";
 import Signup from "./components/SignUp";
 import Login from "./components/Login";
+import { Switch, BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 
 
 function App() {
   return (
-    <div className="App">
-      <img src="app" alt="" />
-      Learn React
-      <Signup />
-      <Login/>
-    </div>
+    <Router>
+      <div className="App">
+        <h1>Flatiron Network</h1>
+
+        <Link to="/login"><button>Login</button></Link>
+        <Link to="/Sign_up"><button>Signup</button></Link>
+
+        <Switch>
+          <Route path="/login" component={Login} />
+          <Route path="/sign_up" component={Signup} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
