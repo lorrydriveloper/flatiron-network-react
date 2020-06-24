32e5e807265402468c063b1c4bf729cb32ab5f98
reddevilcero <reddevil_cero@hotmail.com>
2020-06-24 18:01:45 +0100

    improve componet display

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,13 +1,15 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export default function Section({ linkTo, logo, display }) {
   return (
-    <Link to={linkTo}>
+    <NavLink exact to={linkTo} activeClassName="selectedLink">
       <div className="section__link">
-        <span className={logo}>{logo}</span>
-        <span className={logo}>{display}</span>
+        <div className="link_container">
+          <img src={logo} alt={display} />
+          <h4 className="display">{display}</h4>
+        </div>
       </div>
-    </Link>
+    </NavLink>
   );
 }
