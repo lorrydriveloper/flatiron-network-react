de8f433dde06d6ab783469e2ea6e248691e2be93
reddevilcero <reddevil_cero@hotmail.com>
2020-06-24 10:03:18 +0100

    delete links to login/signup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from "react";
 import "./assets/styles/App.scss";
-// import FirstComponent from "./components/FirstComponent";
-import Signup from "./components/SignUp";
-import Login from "./components/Login";
-import { Switch, BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+import { Switch, BrowserRouter as Router, Route} from "react-router-dom";
 import Home from "./pages/Home";
+import Authpage from "./pages/AuthPage";
 
 function App() {
   return (
@@ -12,18 +11,11 @@ function App() {
       <div className="App">
         <h1>Flatiron Network</h1>
 
-        <Link to="/login">
-          <button>Login</button>
-        </Link>
-        <Link to="/Sign_up">
-          <button>Signup</button>
-        </Link>
-
         <Switch>
-          {/* <Route path='/'  /> */}
-          <Route path="/login" component={Login} />
-          <Route path="/sign_up" component={Signup} />
-          <Route path="/Home" component={Home} />
+          <Route
+            path="/"
+            render={(props) => (localStorage.token ? <Home {...props} /> : <Authpage {...props}/>)}
+          />
         </Switch>
       </div>
     </Router>
