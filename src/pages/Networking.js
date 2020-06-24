62358ab950a716c7d6ac802048337fd7cb64080f
reddevilcero <reddevil_cero@hotmail.com>
2020-06-24 10:02:39 +0100

    create basic Networking,Map and Profile Page

---
+++ src/pages/Networking.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Networking extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Networking PAge</h1>
+      </div>
+    )
+  }
+}
