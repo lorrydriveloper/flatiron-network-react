9683e2202239d0ae0220ca7b2eac187aa23a188c
reddevilcero <reddevil_cero@hotmail.com>
2020-06-24 12:25:53 +0100

    apply protected routes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
 import React from "react";
 import "./assets/styles/App.scss";
 
-import { Switch, BrowserRouter as Router, Route} from "react-router-dom";
+import { Switch, BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Authpage from "./pages/AuthPage";
+import Map from "./pages/Map";
+import Networking from "./pages/Networking";
+import Profile from "./pages/Profile";
+import Protectedroute from "./helpers/ProtectedRoute";
+
 
 function App() {
   return (
     <Router>
       <div className="App">
         <h1>Flatiron Network</h1>
-
         <Switch>
+          <Protectedroute path="/Map" component={Map} />
+          <Protectedroute path="/Networking" component={Networking} />
+          <Protectedroute path="/Profile" component={Profile} />
           <Route
             path="/"
-            render={(props) => (localStorage.token ? <Home {...props} /> : <Authpage {...props}/>)}
+            render={(props) =>
+              localStorage.token ? <Home {...props} /> : <Authpage {...props} />
+            }
           />
         </Switch>
       </div>
