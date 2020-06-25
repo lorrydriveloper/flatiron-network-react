3d7d2d92af3a9ebf94bce84e5777387b59ed9536
reddevilcero <reddevil_cero@hotmail.com>
2020-06-25 20:21:36 +0100

    add pictures to assets

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import "./assets/styles/main.scss";
 import React from "react";
-import { Switch, BrowserRouter as Router, Route } from "react-router-dom";
-import Home from "./pages/Home";
+// import { Switch, BrowserRouter as Router, Route } from "react-router-dom";
+// import Home from "./pages/Home";
 import Authpage from "./pages/AuthPage";
-import Map from "./pages/Map";
-import Networking from "./pages/Networking";
-import Profile from "./pages/Profile";
-import Protectedroute from "./helpers/ProtectedRoute";
+// import Map from "./pages/Map";
+// import Networking from "./pages/Networking";
+// import Profile from "./pages/Profile";
+// import Protectedroute from "./helpers/ProtectedRoute";
 import Header from "./components/Header";
-import Navbar from "./components/Navbar";
+// import Navbar from "./components/Navbar.notused";
 import { connect } from "react-redux";
 
 function App(props) {
   return (
-    <Router>
-      <div className="App">
-        <Route component={Header} />
-        {props.auth ? <Navbar /> : null}
-        <Switch>
+    // <Router>
+    <div className="App">
+      {props.auth ? <Header /> : <Authpage {...props} />}
+      {/* <Switch>
           <Protectedroute path="/Map" component={Map} />
           <Protectedroute path="/Networking" component={Networking} />
           <Protectedroute path="/Profile" component={Profile} />
@@ -27,9 +25,9 @@ function App(props) {
               localStorage.token ? <Home {...props} /> : <Authpage {...props} />
             }
           />
-        </Switch>
-      </div>
-    </Router>
+        </Switch> */}
+    </div>
+    // </Router>
   );
 }
 
