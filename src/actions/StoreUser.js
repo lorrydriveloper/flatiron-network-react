926410c451f969570356e6a6203ef5f6499aeedc
reddevilcero <reddevil_cero@hotmail.com>
2020-06-26 17:16:45 +0100

    use of Thunk to clean components.

--- src/actions/StoreUser.js
+++ src/actions/StoreUser.js
@@ -1,6 +1,27 @@
-export default function(user){
-  return {
-    type: 'STORAGE_USER',
-    payload: user
-  }
-}
+import { BASEURL } from "../helpers/BaseURL";
+
+export const storeUser = (state) => {
+  const configurationObject = {
+    method: "Post",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(state),
+  };
+
+  return (dispatch) => {
+    return fetch(BASEURL + "login", configurationObject)
+      .then((res) => res.json())
+      .then((json) => {
+        if (json.error) {
+          throw new Error(json.error + " " + json.message);
+        }
+        localStorage.setItem("token", json.jwt);
+        dispatch({
+          type: "STORAGE_USER",
+          payload: json.user,
+        });
+      });
+  };
+};
