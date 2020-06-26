ff391d9ddaf498aae11a536e2d5b46a749eba5c8
reddevilcero <reddevil_cero@hotmail.com>
2020-06-26 12:50:46 +0100

    change url to deploy

--- src/helpers/BaseURL.js
+++ src/helpers/BaseURL.js
@@ -1,1 +1,2 @@
-export const BASEURL = "https://flatiron-network-api.herokuapp.com/api/v1/";
+export const BASEURL = "http://localhost:3001/api/v1/";
+// "https://flatiron-network-api.herokuapp.com/api/v1/"
