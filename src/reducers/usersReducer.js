c39a7026e9db62c4d01f95d2354da70da77cb5a2
reddevilcero <reddevil_cero@hotmail.com>
2020-06-29 10:44:47 +0100

    add create and filter user reducer

--- src/reducers/usersReducer.js
+++ src/reducers/usersReducer.js
@@ -1,9 +1,14 @@
 export default function userReducers(
-  state = { user: {}, users: [] },
+  state = { user: {}, users: [], filterUsers: [] },
   { type, payload }
 ) {
   switch (type) {
     case "STORAGE_USER":
+      return {
+        ...state,
+        user: payload,
+      };
+    case "CREATE_USER":
       return {
         ...state,
         user: payload,
@@ -13,8 +18,32 @@ export default function userReducers(
         ...state,
         users: payload,
       };
+    case "FILTER_USERS":
+      return {
+        ...state,
+        filterUsers: filterUsers(state.users, payload),
+      };
 
     default:
       return state;
+  }
+}
+
+function filterUsers(array, filter) {
+  switch (filter.searchBy) {
+    case "cohort":
+      return array.filter((item) => item.cohort.id === filter.cohort);
+    case "course":
+      return array.filter((item) => item.cohort.course.id === filter.course);
+    case "campus":
+      return array.filter((item) => item.cohort.campus.id === filter.campus);
+    case "course & campus":
+      return array.filter(
+        (item) =>
+          item.cohort.campus.id === filter.campus &&
+          item.cohort.course.id === filter.course
+      );
+    default:
+      return array;
   }
 }
