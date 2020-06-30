51ff009e7dff4824e22791b86e8cb11a08ff982d
reddevilcero <reddevil_cero@hotmail.com>
2020-06-30 13:48:59 +0100

    Display meetups with MeetupCard and FakeAPiCall

--- src/pages/Networking.js
+++ src/pages/Networking.js
@@ -1,11 +1,56 @@
 import React, { Component } from "react";
+import Grid from "@material-ui/core/Grid";
+import { withStyles, Typography } from "@material-ui/core";
+import { FakeApiCall } from "../helpers/FakeAPICall";
+import MeetupCard from "../components/MeetupCard.js";
 
-export default class Networking extends Component {
+const styles = () => ({
+  root: {
+    background: "red",
+    height: "100%",
+    overflow: "auto",
+  },
+  header: {
+    paddingBottom: 10,
+    textAlign: "center",
+  },
+});
+
+class Networking extends Component {
   render() {
+    const { classes } = this.props;
     return (
-      <div id="networking" className="main-container">
-        <h1>Networking PAge</h1>
-      </div>
+      <>
+        <Typography className={classes.header} variant="h4">
+          Good Place to Networking
+        </Typography>
+        <Grid container className={classes.root} spacing={2}>
+          {FakeApiCall.map((meetup) => {
+            return (
+              <Grid key={meetup.id} item xs={12} sm={6} md={4}>
+                <MeetupCard
+                  title={meetup.name}
+                  location={meetup.localized_location}
+                  organizerPhoto={meetup.organizer.photo.thumb_link}
+                  eventPhoto={cleanPhoto(meetup)}
+                  description={meetup.description}
+                  eventLink={meetup.link}
+                />
+              </Grid>
+            );
+          })}
+        </Grid>
+      </>
     );
+  }
+}
+
+export default withStyles(styles)(Networking);
+
+function cleanPhoto(meetup) {
+  if (meetup.key_photo) {
+    return meetup.key_photo.photo_link;
+  } else {
+    return "https://secure.meetupstatic.com/photos/event/2/e/a/d/600_450131949.jpeg";
   }
 }
