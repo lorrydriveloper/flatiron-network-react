a0f6258e99e2125bba94fae69323a3bea4f2ecce
reddevilcero <reddevil_cero@hotmail.com>
2020-07-02 10:36:00 +0100

    add container to wrap the child components

--- src/pages/AuthPage.js
+++ src/pages/AuthPage.js
@@ -1,13 +1,26 @@
 import React, { useState } from "react";
 import Login from "../components/Login";
 import SignUp from "../components/SignUp";
+import { Grid, CssBaseline, makeStyles } from "@material-ui/core";
 
+const useStyles = makeStyles({
+  root: {
+    height: "90vh",
+    "align-content": "center",
+    padding: 90,
+  },
+});
 export default function Authpage(props) {
   const [toggle, setToggle] = useState(true);
-
-  return toggle ? (
-    <Login {...props} toggle={setToggle} />
-  ) : (
-    <SignUp {...props} toggle={setToggle} />
+  const classes = useStyles();
+  return (
+    <Grid container component="main" className={classes.root}>
+      <CssBaseline />
+      {toggle ? (
+        <Login {...props} toggle={setToggle} />
+      ) : (
+        <SignUp {...props} toggle={setToggle} />
+      )}
+    </Grid>
   );
 }
